5308f2f907a80f6d154d1091cd4d946214ff7630
Matthias Zaugg <matzaugg@gmail.com>
2016-05-19 14:53:16 +0000

    Changed name wealth to assets

---
+++ src/app/pages/assets/assets.ts
@@ -0,0 +1,30 @@
+import {Page, NavController} from 'ionic-angular';
+import {CaseService, Case} from '../../services/case.service';
+
+@Page({
+  templateUrl: 'build/pages/assets/assets.html',
+})
+export class AssetsPage {
+  case: Case;
+
+  static get parameters() {
+    return [[NavController], [CaseService]];
+  }
+
+  constructor(private nav: NavController, private caseService: CaseService){
+    this.nav = nav;
+    this.case = caseService.getCase();
+  }
+
+
+  // Blocks machen. zB age-block. dieser ist valid, wenn über 25 oder wenn unter und ausbildungsstatus erfasst wurde
+  //Im age-block gibt es also alters-select, sowie 1-2 controls zu erstausbildungssituation
+  
+  showNext(){
+    return false;
+  }
+
+  next(event) {
+    //this.nav.push(EinkommenPage);
+  }
+}
